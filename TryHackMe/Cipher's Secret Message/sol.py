6a16f542dca7ea0d0ddf3ddd8a73356c6b0c3e27
TriesteCitizen <lorenzo.wendland18@gmail.com>
2025-09-03 19:41:39 +0200

    Add solve.py for TryHackMe Chipher's Secret Message

---
+++ TryHackMe/Cipher's Secret Message/sol.py
@@ -0,0 +1,9 @@
+def enc(plaintext):
+    return "".join(
+        chr((ord(c) - (base := ord('A') if c.isupper() else ord('a')) - i) % 26 + base)
+        if c.isalpha() else c
+        for i, c in enumerate(plaintext)
+    )
+
+if __name__ == "__main__":
+    print(enc("a_up4qr_kaiaf0_bujktaz_qm_su4ux_cpbq_ETZ_rhrudm"))
